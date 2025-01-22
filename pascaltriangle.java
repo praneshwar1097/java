35b060883008cc7401f840facfde7af1e197be15
praneshwar1097 <158248027+praneshwar1097@users.noreply.github.com>
2025-01-22 12:19:21 +0530

    Add files via upload

---
+++ pascaltriangle.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int k=1;
+	    Scanner in=new Scanner(System.in);
+	    int n=in.nextInt();
+
+	        for(int i=1;i<=n;i++){
+	            for(int j=1;j<=i;j++)
+	            {
+	                System.out.print(k+" ");
+	                k++;
+	            }
+	            System.out.println();
+	                
+	            }
+	        }
+	}
+
